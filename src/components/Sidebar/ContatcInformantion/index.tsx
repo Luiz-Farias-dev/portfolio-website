2f98d6cd880b339d1b0d765a7232c38710e88ea1
LuNe137 <uizsk8@gmail.com>
2022-08-09 18:07:42 -0300

    taking out the phone number

--- src/components/Sidebar/ContatcInformantion/index.tsx
+++ src/components/Sidebar/ContatcInformantion/index.tsx
@@ -4,13 +4,13 @@ import '../../../styles/components/contactinformation.sass';
 export default function ContactInformation() {
   return (
     <section id="information">
-      <div className='info-card'>
+      {/* <div className='info-card'>
         <AiFillPhone id='phone-icon'/>
         <div>
           <h3>Telefone</h3>
           <p>(85) 99972-7976</p>
         </div>
-      </div>
+      </div> */}
 
       <div className='info-card'>
         <AiOutlineMail id='email-icon'/>
