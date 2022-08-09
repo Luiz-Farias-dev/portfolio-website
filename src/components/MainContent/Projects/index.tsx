00815e8b1ce142f70ef347bae1c9204e79d5fc01
LuNe137 <uizsk8@gmail.com>
2022-08-09 17:57:46 -0300

    fixing a but in slider photo

--- src/components/MainContent/Projects/index.tsx
+++ src/components/MainContent/Projects/index.tsx
@@ -3,7 +3,7 @@ import '../../../styles/components/projects.sass';
 
 export default function Projects() {
   const slides = [
-    {url:'https://luiz-portfolio-website.netlify.app0/slider/aluroniRestaurant.jpg', title:'Aluroni Restaurant', id: 0},
+    {url:'https://luiz-portfolio-website.netlify.app/slider/aluroniRestaurant.jpg', title:'Aluroni Restaurant', id: 0},
     {url:'https://luiz-portfolio-website.netlify.app/slider/pokemonSearcher.jpg', title:'Pokemon Searcher', id: 1},
     {url:'https://luiz-portfolio-website.netlify.app/slider/tasklist.jpg', title:'Task List', id: 2}
   ];
