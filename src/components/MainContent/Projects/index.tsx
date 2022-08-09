9c8b5b3669113b1dfef2a4bc0ef9bda4441fb500
LuNe137 <uizsk8@gmail.com>
2022-08-09 17:50:32 -0300

    update resume and slider photos

--- src/components/MainContent/Projects/index.tsx
+++ src/components/MainContent/Projects/index.tsx
@@ -3,9 +3,9 @@ import '../../../styles/components/projects.sass';
 
 export default function Projects() {
   const slides = [
-    {url:'http://localhost:3000/slider/aluroniRestaurant.jpg', title:'Aluroni Restaurant', id: 0},
-    {url:'http://localhost:3000/slider/pokemonSearcher.jpg', title:'Pokemon Searcher', id: 1},
-    {url:'http://localhost:3000/slider/tasklist.jpg', title:'Task List', id: 2}
+    {url:'https://luiz-portfolio-website.netlify.app0/slider/aluroniRestaurant.jpg', title:'Aluroni Restaurant', id: 0},
+    {url:'https://luiz-portfolio-website.netlify.app/slider/pokemonSearcher.jpg', title:'Pokemon Searcher', id: 1},
+    {url:'https://luiz-portfolio-website.netlify.app/slider/tasklist.jpg', title:'Task List', id: 2}
   ];
 
   return (
